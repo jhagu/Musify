3b5684cdd87eb023872d19c091443ce2db6f87e9
jhagu <juan.h.agu@gmail.com>
2017-05-31 22:16:59 -0300

    userController modificado
    Se añadieron metodos de login, update, uploadImage, getImageFIle a userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
 'use strict'
-
+var fs = require("fs");
+var path = require("path");
 var User = require("../models/user.js");
 var bcrypt = require("bcrypt-nodejs");
+var jwt = require("../services/jwt.js");
 
 function pruebas(req, res){
     res.status(200).send({
@@ -17,7 +19,7 @@ function saveUser(req, res){
     user.lastName = params.lastName;
     user.email = params.email;
     user.password = params.password;
-    user.role = "ROLE_ADMIN";
+    user.role = "ROLE_USER";
     user.image = "null";
     
     if (params.password){
@@ -48,7 +50,121 @@ function saveUser(req, res){
     }
 }
 
+function loginUser(req, res){
+    var params = req.body;
+
+    var email = params.email;
+    var password = params.password;
+
+    console.log(params);
+
+    User.findOne({email: email.toLowerCase()}, (err,user) => {
+        if (err){
+            res.status(500).send({message: "Error en la petición"});
+        }
+        else{
+            if(!user){
+                res.status(404).send({message: "El usuario no existe"});
+            }
+            else{
+                //Comprobar contraseña
+                bcrypt.compare(password, user.password, function(err, check){
+                    if (check){
+                        //Datos del usuario logueado,getHash se manda en PostMan
+                        if (params.getHash){
+                            res.status(200).send({token: jwt.createToken(user)});
+                        }
+                        else{
+                            res.status(200).send({user: user});
+                        }     
+                    }
+                    else{
+                        res.status(404).send({message: "El usuario no ha podido loguearse"});
+                    }
+                });
+            }
+        }
+    });
+}
+
+function updateUser(req, res){
+
+    var userId = req.params.id;
+    var update = req.body;
+
+    User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+        if (err){
+            res.status(500).send({message: "Error al actualizar el usuario"});
+        }
+        else{
+            if (!userUpdated){
+                res.status(494).send({message: "No se ha podido actualizar el usuario"});
+            }
+            else{
+                res.status(200).send({user: userUpdated})
+            }
+        }
+    });
+}
+
+function uploadImage(req, res){
+    var userId = req.params.id;
+    var fileName = "No subido...";
+
+    //files --> variable global
+    if (req.files){
+        var filePath = req.files.image.path;
+        var fileSplit = filePath.split("\\"); //Devuelve un array con cada elemento dsp de cada barra
+        var fileName = fileSplit[2]; //Nombre de la imagen
+
+        //Controlamos la extension de la imagen
+        var extSplit = fileName.split("\.");
+        var filExt = extSplit[1];
+
+        if (filExt == "jpg" || filExt == "png" || filExt == "gif"){
+            User.findByIdAndUpdate(userId, {image: fileName}, (err, userUpdated) => {
+                if (err){
+                    res.status(500).send({message: "Error al actualizar el usuario"});
+                }
+                else{
+                    if (!userUpdated){
+                        res.status(494).send({message: "No se ha podido actualizar la imagen del usuario"});
+                    }
+                    else{
+                        res.status(200).send({user: userUpdated})
+                    }
+                }
+            });
+        }
+        else{
+            res.status(200).send({message: "Extesion incorrecta. Suba una imagen con extension jpg, png o gif"});
+        }
+    }else{
+        res.status(200).send({message: "La imagen no se ha subido"});
+    }
+}
+
+function getImageFile(req, res){
+    var imageFile = req.params.imageFile;
+    var pathFile = "./uploads/users/" + imageFile;
+
+    fs.exists(pathFile, function(exists){
+        if (exists){
+            console.log()
+            res.sendFile(path.resolve(pathFile));
+        }
+        else{
+            res.status(200).send({message: "No existe la imagen."});
+        }
+
+    });
+}
+
 module.exports = {
     pruebas,
-    saveUser
+    saveUser,
+    loginUser,
+    updateUser,
+    uploadImage,
+    getImageFile
 }
