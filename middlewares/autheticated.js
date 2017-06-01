e4b1c488589dd47709840a017ccde676a7222245
jhagu <juan.h.agu@gmail.com>
2017-05-31 22:17:47 -0300

    Carpeta middlewares añadida
    Se añadio la carpeta middlewares

---
+++ middlewares/autheticated.js
@@ -0,0 +1,26 @@
+var jwt = require("jwt-simple");
+var moment = require("moment");
+var secret = "you_shall_not_pass";
+
+exports.ensureAuth = function(req, res, next){
+    if (!req.headers.authorization){
+        return res.status(403).send({message: "La petición no tiene la cabecera de autenticacioón"});
+    }
+    //Eliminamos las comillas del token
+    var token = req.headers.authorization.replace(/['"]/g, "");
+
+    try{
+        var payload = jwt.decode(token, secret);
+        if (payload.expirationDate <= moment().unix()){
+            return res.status(401).send({message: "El token ha expirado"});
+        }
+
+    }catch(ex){
+       // console.log(ex);
+        return res.status(404).send({message: "Token no válido"});
+    }
+
+    //Si todo bien, tenemos los datos del usuario en el payload
+    req.user = payload;
+    next(); 
+};
