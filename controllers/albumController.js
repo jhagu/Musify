dcce89c495497128875d977a12ba6be4637394f2
jhagu <juan.h.agu@gmail.com>
2017-06-06 07:37:06 -0300

    albumController y routes finalizados

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -51,7 +51,142 @@ function saveAlbum(req, res){
 
 }
 
+function getAll(req, res){
+    var artistId = req.params.artistId;
+    var find = null;
+    if (!artistId){
+        //Sacar todos los albums de  la bbdd
+        find = Album.find({}).sort("title");
+    }
+    else{
+        //Sacar los albums de un artista concreto
+        find = Album.find({"artist" : artistId}).sort("year");
+    }
+
+    find.populate({path: "artist"}).exec((err, albums) => {
+        if (err){
+            res.status(500).send({message: "Error en la peticion"});
+        }
+        else{
+            if (!albums){
+                res.status(404).send({message: "No existen albums en la base de datos"});
+            }
+            else{
+                res.status(200).send({message: "Albums encontrados", albums: albums});
+            }
+        }
+    });
+}
+
+function updateAlbum(req,res){
+    var albumId = req.params.albumId;
+    var update = req.body;
+
+    Album.findByIdAndUpdate(albumId, update,(err, albumUpdated) => {
+        if (err){
+            res.status(500).send({message: "Error en la petición"});
+        }
+        else{
+            if (!albumUpdated){
+                res.status(404).send({message: "No existe el album en la base de datos"});
+            }
+            else{
+                res.status(200).send({message: "Album que se actualizo", album: albumUpdated});
+            }
+        }
+    });
+
+}
+
+function deleteAlbum(req, res){
+    var albumId = req.params.albumId;
+
+    Album.findByIdAndRemove(albumId, function(err, albumRemoved){
+        if (err){
+            res.status(500).send({message: "Error en la petición"});
+        }
+        else{
+            if (!albumRemoved){
+                res.status(404).send({message: "No se ha podido eliminar el album"});
+            }
+            else{
+                Song.find({album : albumRemoved._id}).remove((err, songRemoved) => {
+                    if (err){
+                        res.status(500).send({message: "Error al eliminar la canción"});
+                    }
+                    else{
+                        if (!songRemoved){
+                            res.status(404).send({message: "No se ha podido eliminar la canción"});
+                        }
+                        else{
+                            res.status(200).send({message: "Album eliminado", album : albumRemoved});
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
+
+function uploadImage(req, res){
+    var albumId = req.params.albumId;
+    var fileName = "No subido...";
+
+    //files --> variable global
+    if (req.files){
+        var filePath = req.files.image.path;
+        var fileSplit = filePath.split("\\"); //Devuelve un array con cada elemento dsp de cada barra
+        var fileName = fileSplit[2]; //Nombre de la imagen
+
+        //Controlamos la extension de la imagen
+        var extSplit = fileName.split("\.");
+        var filExt = extSplit[1];
+
+        if (filExt == "jpg" || filExt == "png" || filExt == "gif"){
+            Album.findByIdAndUpdate(albumId, {image: fileName}, (err, albumUpdated) => {
+                if (err){
+                    res.status(500).send({message: "Error en la petición"});
+                }
+                else{
+                    if (!albumUpdated){
+                        res.status(404).send({message: "No se ha podido actualizar la imagen del album"});
+                    }
+                    else{
+                        res.status(200).send({message: "Imagen de album actualizada", album: albumUpdated})
+                    }
+                }
+            });
+        }
+        else{
+            res.status(200).send({message: "Extesion incorrecta. Suba una imagen con extension jpg, png o gif"});
+        }
+    }else{
+        res.status(200).send({message: "La imagen no se ha subido"});
+    }
+}
+
+function getImageFile(req, res){
+    var imageFile = req.params.imageFile;
+    var pathFile = "./uploads/albums/" + imageFile;
+
+    fs.exists(pathFile, function(exists){
+        if (exists){
+            console.log()
+            res.sendFile(path.resolve(pathFile));
+        }
+        else{
+            res.status(200).send({message: "No existe la imagen."});
+        }
+
+    });
+}
+
 module.exports = {
     getAlbum,
-    saveAlbum
+    saveAlbum,
+    getAll,
+    updateAlbum,
+    deleteAlbum,
+    uploadImage,
+    getImageFile
 }
--- routes/albumRoutes.js
+++ routes/albumRoutes.js
@@ -4,15 +4,15 @@ var express = require("express");
 var albumController = require("../controllers/albumController.js");
 var multiparty = require("connect-multiparty");
 var md_auth = require("../middlewares/autheticated.js");
-var md_upload = multiparty({uploadDir: "./uploads/album"});
+var md_upload = multiparty({uploadDir: "./uploads/albums"});
 var api = express.Router();
 
 api.get("/getAlbum/:id", md_auth.ensureAuth, albumController.getAlbum);
 api.post("/saveAlbum", md_auth.ensureAuth, albumController.saveAlbum);
-//api.get("/getAll/:page?", md_auth.ensureAuth, artistController.getAll);
-//api.put("/updateArtist/:id", md_auth.ensureAuth, artistController.updateArtist);
-//api.delete("/deleteArtist/:id", md_auth.ensureAuth, artistController.deleteArtist);
-//api.post("/upload-image/:id", [md_auth.ensureAuth, md_upload], artistController.uploadImage);
-//api.get("/get-image/:imageFile", artistController.getImageFile);
+api.get("/getAll/:artistId?", md_auth.ensureAuth, albumController.getAll);
+api.put("/updateAlbum/:albumId", md_auth.ensureAuth, albumController.updateAlbum);
+api.delete("/deleteAlbum/:albumId", md_auth.ensureAuth, albumController.deleteAlbum);
+api.post("/upload-image/:albumId", [md_auth.ensureAuth, md_upload], albumController.uploadImage);
+api.get("/get-image/:imageFile", albumController.getImageFile);
 
 module.exports = api;
