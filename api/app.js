871ceafce3d6fb562e77c4eb27d525777360246d
jhagu <juan.h.agu@gmail.com>
2017-06-08 13:07:09 -0300

    modficaciones

--- api/app.js
+++ api/app.js
@@ -28,6 +28,8 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+
+    next();
 });
 
 
